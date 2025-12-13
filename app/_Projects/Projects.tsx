a771a697329df63fb30da9aabee6e3e24202f385
NZYM <enzsama.dev@gmail.com>
2025-12-13 11:10:51 +0100

    Add Routana details to project section

--- app/_Projects/Projects.tsx
+++ app/_Projects/Projects.tsx
@@ -7,9 +7,9 @@ const projects = [
     description:
       "A real-time collaborative platform for creating, editing, and organizing documentation and notes across teams.",
     fullDescription: `
-      A real-time collaborative platform for creating, editing, and organizing 
-      documentation and notes. Multiple users can work together simultaneously and share 
-      updates instantly. The system ensures smooth synchronization, structured 
+      A real-time collaborative platform for creating, editing, and organizing
+      documentation and notes. Multiple users can work together simultaneously and share
+      updates instantly. The system ensures smooth synchronization, structured
       organization, and an intuitive writing experience designed for both individuals and teams.
       `,
     stack: [
@@ -31,22 +31,49 @@ const projects = [
     description:
       "A secure, session aware authentication system for typescript applications.",
     fullDescription: `
-      A robust typescript authentication service featuring secure user login, session 
-      tracking, and token-based access control. Users can sign in, maintain active 
-      sessions, and refresh access seamlessly. The system ensures reliable security, 
+      A robust typescript authentication service featuring secure user login, session
+      tracking, and token-based access control. Users can sign in, maintain active
+      sessions, and refresh access seamlessly. The system ensures reliable security,
       smooth authentication flow, and efficient session management across all user interactions.
-      There's also a react app that demonstrate how developers can best implement 
+      There's also a react app that demonstrate how developers can best implement
       NodeAuth with their client application. <a href="https://github.com/absterr/AuthClient"
       target="_blank" rel="noopener noreferrer"
       style="color:#22c55e; text-decoration:underline;">
       Click here for more.
-      </a> 
+      </a>
       `,
     stack: ["Node.js", "Typescript", "Express", "PostgreSQL", "Sequelize"],
     codeUrl: "https://github.com/absterr/NodeAuth",
     previewUrl: null,
     videoDemo: null,
   },
+  {
+    title: "ROUTANA",
+    image: "https://res.cloudinary.com/dlqecula3/image/upload/v1765617642/Screenshot_193_sycbjf.png",
+    description:
+      "A personalized learning roadmap generator that helps users plan, track, and adapt their journey based on goals and skill level.",
+    fullDescription: `
+      A personalized learning roadmap generator designed to help users build clear,
+      goal-driven learning paths. The platform analyzes user goals, current skill level,
+      and time availability to generate structured roadmaps with milestones, resources,
+      and progress tracking. Roadmaps adapt over time based on progress and feedback,
+      making it easier for individuals to stay focused and learn efficiently.
+    `,
+    stack: [
+      "Node.js",
+      "Typescript",
+      "Express",
+      "PostgreSQL",
+      "DrizzleORM",
+      "React",
+      "Bun",
+      "Tanstack React query",
+      "Docker"
+    ],
+    codeUrl: "https://github.com/absterr/Routana",
+    previewUrl: "https://routana.onrender.com",
+    videoDemo: "https://res.cloudinary.com/dlqecula3/video/upload/v1765618696/Routana_demo_cq7nus.mp4"
+  }
 ];
 
 const Projects = () => {
